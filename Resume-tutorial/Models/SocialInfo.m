4abe5aa1fe481b7d07462a1b4e5acb2104f16250
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-07-09 20:11:50 -0700

    added tab bar controller

---
+++ Resume-tutorial/Models/SocialInfo.m
@@ -0,0 +1,13 @@
+//
+//  SocialInfo.m
+//  Resume-tutorial
+//
+//  Created by Stephanie Szeto on 7/9/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import "SocialInfo.h"
+
+@implementation SocialInfo
+
+@end
