f2749a674331b08e936106701d5341638019e07d
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-07-10 00:00:29 -0700

    new social info model

--- Resume-tutorial/Models/SocialInfo.m
+++ Resume-tutorial/Models/SocialInfo.m
@@ -10,4 +10,27 @@
 
 @implementation SocialInfo
 
++ (SocialInfo *)socialInfoWithtTitle:(NSString *)title iconNamed:(NSString *)iconName url:(NSString *)url {
+    SocialInfo *info = [self new];
+    info.title = title;
+    info.icon = [UIImage imageNamed:iconName];
+    info.url = url;
+    
+    return info;
+}
+
++ (NSArray *)allSocialInfo {
+    static NSArray *all;
+    
+    if (!all) {
+        all = @[[self socialInfoWithtTitle:@"GitHub" iconNamed:@"GitHubIcon" url:@"http://github.com/stephanieszeto"],
+                
+                [self socialInfoWithtTitle:@"LinkedIn" iconNamed:@"LinkedInIcon" url:@"http://linkedin.com/in/stephanieszeto"],
+                
+                [self socialInfoWithtTitle:@"Twitter" iconNamed:@"TwitterIcon" url:@"http://twitter.com/stephszeto"]];
+    }
+    
+    return all;
+}
+
 @end
