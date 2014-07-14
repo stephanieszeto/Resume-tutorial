3c42ec3424dd1873e99fe860168bc12b78b550a2
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-07-13 17:50:20 -0700

    fixed typo

--- Resume-tutorial/Models/SocialInfo.m
+++ Resume-tutorial/Models/SocialInfo.m
@@ -10,7 +10,7 @@
 
 @implementation SocialInfo
 
-+ (SocialInfo *)socialInfoWithtTitle:(NSString *)title iconNamed:(NSString *)iconName url:(NSString *)url {
++ (SocialInfo *)socialInfoWithTitle:(NSString *)title iconNamed:(NSString *)iconName url:(NSString *)url {
     SocialInfo *info = [self new];
     info.title = title;
     info.icon = [UIImage imageNamed:iconName];
@@ -23,14 +23,15 @@
     static NSArray *all;
     
     if (!all) {
-        all = @[[self socialInfoWithtTitle:@"GitHub" iconNamed:@"GitHubIcon" url:@"http://github.com/stephanieszeto"],
+        all = @[[self socialInfoWithTitle:@"GitHub" iconNamed:@"GitHubIcon" url:@"http://github.com/stephanieszeto"],
                 
-                [self socialInfoWithtTitle:@"LinkedIn" iconNamed:@"LinkedInIcon" url:@"http://linkedin.com/in/stephanieszeto"],
+                [self socialInfoWithTitle:@"LinkedIn" iconNamed:@"LinkedInIcon" url:@"http://linkedin.com/in/stephanieszeto"],
                 
-                [self socialInfoWithtTitle:@"Twitter" iconNamed:@"TwitterIcon" url:@"http://twitter.com/stephszeto"]];
+                [self socialInfoWithTitle:@"Twitter" iconNamed:@"TwitterIcon" url:@"http://twitter.com/stephszeto"]];
     }
     
     return all;
 }
 
 @end
+
